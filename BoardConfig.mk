6225bd97ff1a9bc1b57960181a70e028f7f6e5ed
Erez A. Korn <erakorn@gmail.com>
2012-09-11 21:31:05 +0300

    Changed to franco kernel config
    Removed kernel binary
    Moved kernel out of tree
    
    Change-Id: I9450fcb905b7a166e32156106ed758a1431adae1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,10 +33,8 @@ BOARD_KERNEL_BASE := 0x80000000
 # BOARD_KERNEL_CMDLINE :=
 
 # Define kernel config for inline building
-TARGET_KERNEL_CONFIG := cyanogenmod_tuna_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/tuna
-
-TARGET_PREBUILT_KERNEL := device/samsung/tuna/kernel
+TARGET_KERNEL_CONFIG := franco_defconfig
+TARGET_KERNEL_SOURCE := ../external/franco_tuna
 
 TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := omap4
